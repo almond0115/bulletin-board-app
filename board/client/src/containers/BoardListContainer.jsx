f8bf1dd56df4da38052c3d0b72e3a07f579f7120
JongHyunJung <jjh19960115@gmail.com>
2023-12-04 21:30:38 +0900

    feat: board list

--- board/client/src/containers/BoardListContainer.jsx
+++ board/client/src/containers/BoardListContainer.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import * as boards from '../apis/boards'
+import BoardList from '../components/BoardList'
 
 // 게시글 목록
 const BoardListContainer = () => {
-  return (
-    <div>
-        <h1>게시글 목록</h1>
-        <Link to="/boards/insert">글쓰기</Link>
-        <hr />
-        <Link to="/boards/10">게시글 조회(10)</Link>
-    </div>
-  )
+
+  // state 설정
+  const [boardList, setBoardList] = useState([])
+
+  // 게시글 목록 데이터
+  const getBoardList = async () => {
+    const response = await boards.list();
+    const data = await response.data;
+    console.log(data);
+    setBoardList(data)
+  };
+
+  useEffect( () => {
+    getBoardList();
+  }, [])
+
+  return <BoardList boardList={boardList}/>
 }
 
 export default BoardListContainer
